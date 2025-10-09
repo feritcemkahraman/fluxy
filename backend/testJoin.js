6dc9385b2ab5a32fe546d90d623d7d11ba1ceaa6
feritcemkahraman <frtcm11@gmail.com>
2025-10-10 01:36:44 +0300

    ngrok url

--- backend/testJoin.js
+++ backend/testJoin.js
@@ -10,7 +10,7 @@ const testJoinServer = async () => {
     console.log('Server ID:', serverId);
     
     const response = await axios.post(
-      `https://e54208101c71.ngrok-free.app/api/servers/${serverId}/join`,
+      `https://2618e48ceb3b.ngrok-free.app/api/servers/${serverId}/join`,
       {},
       {
         headers: {
